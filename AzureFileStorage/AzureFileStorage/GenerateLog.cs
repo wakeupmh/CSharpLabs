573f9853a9351f26a22c4297e718510888dd6c9b
Marcos Henrique Santos de Lima <marcos.lima@agrotools.local>
2019-08-20 15:17:07 -0300

    CreateIfNotExists share, read, write

---
+++ AzureFileStorage/AzureFileStorage/GenerateLog.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFileStorage
+{
+    class GenerateLog
+    {
+        private CloudStorageAccount StorageAccount { get; set; }
+        private CloudFileShare Share { get; set; }
+        private CloudFile File { get; set; }
+        public GenerateLog() {}
+        public async Task Initialize(string owner, string connectionString)
+        {
+            if (string.IsNullOrEmpty(owner))
+            {
+                owner = "UNDEFINED";
+            }
+            await FileShareClient($"{owner.ToLower()}logs", connectionString);
+        }
+        private async Task FileShareClient(string shareReferenceName, string connectionString)
+        {
+            StorageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudFileClient fileClient = StorageAccount.CreateCloudFileClient();
+            Share = fileClient.GetShareReference(shareReferenceName);
+            await Share.CreateIfNotExistsAsync();
+        }
+        public async Task<string> GetLogContent()
+        {
+            try
+            {
+                CloudFileDirectory root = Share.GetRootDirectoryReference();
+                File = root.GetFileReference($"log_{DateTime.Now.ToString("dd_MM_yyyy")}.log");
+                string content;
+                if (!await File.ExistsAsync())
+                {
+                    content = "";
+                }
+                else
+                {
+
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await File.DownloadToStreamAsync(memoryStream);
+                        content = Encoding.UTF8.GetString(memoryStream.ToArray());
+                    }
+
+
+                }
+                return content;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public async Task WriteLog(string text)
+        {
+            try
+            {
+                var actualContent = await GetLogContent();
+                actualContent += text;
+                using (var memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(Encoding.UTF8.GetBytes(actualContent), 0, Encoding.UTF8.GetBytes(actualContent).Length);
+                    memoryStream.Position = 0;
+                    await File.UploadFromStreamAsync(memoryStream);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+    }
+}
--- AzureFileStorage/AzureFileStorage/StoreFile.cs
+++ AzureFileStorage/AzureFileStorage/StoreFile.cs
@@ -18,48 +18,53 @@ using Microsoft.Azure.WebJobs.Host;
 
 namespace AzureFileStorage
 {
-    public static class StoreFile
+    public class StoreFile
     {
         [FunctionName("StoreFile")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestMessage req, ILogger log)
+        public async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequestMessage req, ILogger log)
         {
-            await CreateFile(log);
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=atgeologs;AccountKey=nOL6CTcURztrP/qu31pc/PZTZffyrb+TlFNtZqaCF7fePQzGE9r6EpjcdDY8QdSYLbrxaIdT/QWcKnoxGVaQ3w==;EndpointSuffix=core.windows.net";
+
+            await CreateFile(connectionString);
 
             return req.CreateResponse(HttpStatusCode.OK, string.Empty);
         }
-        private async static Task CreateFile(ILogger log)
+        private async Task CreateFile(string connectionString)
         {
-            string connectionString;
-            CloudStorageAccount storageAccount;
-            connectionString = "DefaultEndpointsProtocol=https;AccountName=<yourAccountName>;AccountKey=<yourKey>;EndpointSuffix=core.windows.net";
-            storageAccount = CloudStorageAccount.Parse(connectionString);
-            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
-            CloudFileShare share = fileClient.GetShareReference("<yourReference>");
-            await share.CreateIfNotExistsAsync();
+            //string connectionString;
+            //CloudStorageAccount storageAccount;
+            //storageAccount = CloudStorageAccount.Parse(connectionString);
+            //CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+            //CloudFileShare share = fileClient.GetShareReference("atfilelogs");
+            //await share.CreateIfNotExistsAsync();
             try
             {
-                CloudFileDirectory root = share.GetRootDirectoryReference();
-                var file = root.GetFileReference("test.txt");
-                string oldContent;
-                if (!await file.ExistsAsync())
-                {
-                    oldContent = "";
-                }
-                else
-                {
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        await file.DownloadToStreamAsync(memoryStream);
-                        oldContent = Encoding.UTF8.GetString(memoryStream.ToArray());
-                    }
-                }
-                oldContent += "New Content\n";
-                using (var memoryStream = new MemoryStream())
-                {
-                    memoryStream.Write(Encoding.UTF8.GetBytes(oldContent), 0, Encoding.UTF8.GetBytes(oldContent).Length);
-                    memoryStream.Position = 0;
-                    await file.UploadFromStreamAsync(memoryStream);
-                }
+                GenerateLog genLog = new GenerateLog();
+                await genLog.Initialize("tmx", connectionString);
+                await genLog.WriteLog("TESTE\n");
+             
+                //CloudFileDirectory root = share.GetRootDirectoryReference();
+                //var file = root.GetFileReference("teste.txt");
+                //string oldContent;
+                //if (!await file.ExistsAsync())
+                //{
+                //    oldContent = "";
+                //}
+                //else
+                //{
+                //    using (var memoryStream = new MemoryStream())
+                //    {
+                //        await file.DownloadToStreamAsync(memoryStream);
+                //        oldContent = Encoding.UTF8.GetString(memoryStream.ToArray());
+                //    }
+                //}
+                //oldContent += "Novo conteudo\n";
+                //using (var memoryStream = new MemoryStream())
+                //{
+                //    memoryStream.Write(Encoding.UTF8.GetBytes(oldContent), 0, Encoding.UTF8.GetBytes(oldContent).Length);
+                //    memoryStream.Position = 0;
+                //    await file.UploadFromStreamAsync(memoryStream);
+                //}
             }
             catch (Exception ex)
             {
